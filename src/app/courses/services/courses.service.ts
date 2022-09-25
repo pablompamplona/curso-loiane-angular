3c54462bdf698654325dc1084c7a6f648725ca31
Pablo Alexandre Pamplona <pablompamplona@gmail.com>
2022-09-25 00:16:29 -0300

    first commit

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = 'assets/courses.json'
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+  }
+}

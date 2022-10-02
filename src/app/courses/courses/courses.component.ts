6412b3af663e0d9d283f0a8df2c2636be613d7ea
Pablo Alexandre Pamplona <pablompamplona@gmail.com>
2022-10-02 18:59:36 -0300

    course component

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, delay, Observable, of } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-
-
-  constructor(private coursesService: CoursesService) {
-    this.courses = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      delay(1500),
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.');
+        return of([]);
+      })
+    );
   }
 
-  ngOnInit(): void {
-
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
+  ngOnInit(): void { }
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
+
+
+@NgModule({
+  declarations: [
+    ErrorDialogComponent,
+    CategoryPipe
+  ],
+  imports: [
+    CommonModule,
+    AppMaterialModule
+  ],
+  exports: [
+    ErrorDialogComponent,
+    CategoryPipe
+  ]
+})
+export class SharedModule { }
